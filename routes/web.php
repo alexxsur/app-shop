b0db37604b517ad579d7c84e182935dc726a581b
Alex <alexxsur@hotmail.com>
2018-02-10 16:11:07 -0500

    Crud de productos

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'TestController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/products', 'ProductController@index');  //listado
+Route::get('/admin/products/create', 'ProductController@create'); // formulario crear
+Route::post('/admin/products', 'ProductController@store'); //crear
