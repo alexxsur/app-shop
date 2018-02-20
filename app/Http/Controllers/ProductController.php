dd62510c1976f2d3f270e4d61102957134773037
Alex <alexxsur@hotmail.com>
2018-02-19 21:56:58 -0500

    Entidades Cart, CartDetail y Namespaces

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,102 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Product;
-use App\ProductImage;
 
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
-     $products = Product::paginate(10);
-     return view('admin.products.index')->with(compact('products')); //listado
-    }
-
-    public function create()
-    {
-     return view('admin.products.create'); //formulario de registro
-    }
-
-    public function store(Request $request)
-    {
-        //Validar
-        $messages = [
-        	'name.required' => 'Es necesario ingresar un nombre para el producto.',
-        	'name.min' => 'El nombre del producto debe tener al menos 3 caracteres.',
-        	'description.required' => 'La descripción corta es un campo obligatorio.',        	
-        	'description.max' => 'La descripción corta solo admite hasta 200 caracteres.',
-        	'price.required' => 'Es obligatorio definir el precio para el producto.',
-        	'price.numeric' => 'Ingrese un precio válido.',
-        	'price.min' => 'No se admiten valores negativos.'
-        ];    	
-        //Validar
-        $rules = [
-        	'name' => 'required|min:3',
-        	'description' => 'required|max:200',
-        	'price' => 'required|numeric|min:0'
-        ];
-       $this->validate($request, $rules, $messages);
-
-      //registrar el nuevo producto en la BD
-      //dd($request->all());
-    	$product = new Product();
-    	$product->name = $request->input('name');
-    	$product->description = $request->input('description');
-    	$product->price = $request->input('price');
-    	$product->long_description = $request->input('long_description');
-    	$product->save(); //INSERT
-
-    	return redirect('/admin/products');
-    }
-
-    public function edit($id)
-    {
-     //return 'Mostrar el producto $id '.$id;
-     $product = Product::find($id);
-     return view('admin.products.edit')->with(compact('product')); //formulario de edición
-    }
-
-    public function update(Request $request, $id)
-    {
-        //Validar
-        $messages = [
-        	'name.required' => 'Es necesario ingresar un nombre para el producto.',
-        	'name.min' => 'El nombre del producto debe tener al menos 3 caracteres.',
-        	'description.required' => 'La descripción corta es un campo obligatorio.',        	
-        	'description.max' => 'La descripción corta solo admite hasta 200 caracteres.',
-        	'price.required' => 'Es obligatorio definir el precio para el producto.',
-        	'price.numeric' => 'Ingrese un precio válido.',
-        	'price.min' => 'No se admiten valores negativos.'
-        ];    	
-        //Validar
-        $rules = [
-        	'name' => 'required|min:3',
-        	'description' => 'required|max:200',
-        	'price' => 'required|numeric|min:0'
-        ];
-       $this->validate($request, $rules, $messages);    	
-      //Actualizar producto en la BD      
-    	$product = Product::find($id);
-    	$product->name = $request->input('name');
-    	$product->description = $request->input('description');
-    	$product->price = $request->input('price');
-    	$product->long_description = $request->input('long_description');
-    	$product->save(); //UPDATE
-
-    	return redirect('/admin/products');
-    }
-
-    public function destroy($id)
-    {
-       
-	    //CartDetail::where('product_id', $id)->delete();/*TEMPORAL PARA PREVENIR ERROR DE DEPENDENCIAS AL BORRAR PRODUCTOS*/
-	    ProductImage::where('product_id', $id)->delete();/*TEMPORAL PARA PREVENIR ERROR DE DEPENDENCIAS AL BORRAR PRODUCTOS*/
-
-      //Borrar producto en la BD      
-    	$product = Product::find($id);
-    	$product->delete(); //DELETE
-    	return back();
-    }       
+   public function show($id){
+   	  return "Mostrar datos del producto $id";
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ Route::get('/', 'TestController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/products/{id}', 'ProductController@show');
 
-Route::middleware(['auth','admin'])->prefix('admin')->group(function () {
+Route::middleware(['auth','admin'])->prefix('admin')->namespace('admin')->group(function () {
 	Route::get('/products', 'ProductController@index');  //listado
 	Route::get('/products/create', 'ProductController@create'); // formulario crear
 	Route::post('/products', 'ProductController@store'); //crear
