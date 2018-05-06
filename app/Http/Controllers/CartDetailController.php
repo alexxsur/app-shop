357c71dd0edfe5c8e62c79927c0c6882553a7aa9
Alex <alexxsur@hotmail.com>
2018-05-06 09:32:28 -0500

    Set middleware auth for cart details

--- app/Http/Controllers/CartDetailController.php
+++ app/Http/Controllers/CartDetailController.php
@@ -8,24 +8,31 @@ use App\CartDetail;
 
 class CartDetailController extends Controller
 {
-  public function store(Request $request)
+    public function __construct()
     {
-    	$cartDetail = new CartDetail();
-    	$cartDetail->cart_id = auth()->user()->cart->id;
-    	$cartDetail->product_id = $request->product_id;
-    	$cartDetail->quantity = $request->quantity;
-    	$cartDetail->save();
-		$notification = 'El producto se ha cargado a tu carrito de compras exitosamente!';
-    	return back()->with(compact('notification'));
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request)
+    {
+        $cartDetail = new CartDetail();
+        $cartDetail->cart_id = auth()->user()->cart->id;
+        $cartDetail->product_id = $request->product_id;
+        $cartDetail->quantity = $request->quantity;
+        $cartDetail->save();
+
+        $notification = 'El producto se ha cargado a tu carrito de compras exitosamente!';
+        return back()->with(compact('notification'));
     }
 
     public function destroy(Request $request)
     {
-    	$cartDetail = CartDetail::find($request->cart_detail_id);
-    	
-    	if ($cartDetail->cart_id == auth()->user()->cart->id)
-    		$cartDetail->delete();
-    	$notification = 'El producto se ha eliminado del carrito de compras correctamente.';
-    	return back()->with(compact('notification'));
-    }    
+        $cartDetail = CartDetail::find($request->cart_detail_id);
+
+        if ($cartDetail->cart_id == auth()->user()->cart->id)
+            $cartDetail->delete();
+
+        $notification = 'El producto se ha eliminado del carrito de compras correctamente.';
+        return back()->with(compact('notification'));
+    }
 }
